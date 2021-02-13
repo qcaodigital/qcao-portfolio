a9307657bef69d49fe56c1c4003da4f9b4c2534f
Quan Cao <qcaodigital@gmail.com>
2021-02-13 01:33:03 -0500

    add transitions to about page

--- src/pages/About/About.transitions.ts
+++ src/pages/About/About.transitions.ts
@@ -1,31 +1,62 @@
 const defaultTranslate: number = window.innerHeight * 0.75;
 const defaultDuration: number = 0.75;
 
-export const transitions = {
-	landing: {
-		top: {
-			y: defaultTranslate,
-			opacity: 0,
-			transition: {
-				duration: defaultDuration,
-				ease: 'easeIn',
-			},
-		},
-		animate: {
-			y: 0,
-			opacity: 1,
-			transition: {
-				duration: defaultDuration,
-				ease: 'easeOut',
-			},
-		},
-		bottom: {
-			y: defaultTranslate * -1,
-			opacity: 0,
-			transition: {
-				duration: defaultDuration,
-				ease: 'easeIn',
-			},
-		},
-	},
+interface variants {
+    top: {};
+    animate: {};
+    bottom: {};
+    [key: string]: {};
+}
+
+interface transitionsType {
+    stagger: variants;
+    transUp: variants;
+}
+
+export const transitions: transitionsType = {
+    stagger: {
+        top: {
+            transition: {
+                staggerChildren: 0.05,
+                staggerDirection: -1,
+            },
+        },
+        animate: {
+            transition: {
+                staggerChildren: 0.05,
+            },
+        },
+        bottom: {
+            transition: {
+                staggerChildren: 0.05,
+                staggerDirection: 1,
+            },
+        },
+    },
+    transUp: {
+        top: {
+            y: defaultTranslate,
+            opacity: 0,
+            transition: {
+                duration: defaultDuration,
+                ease: "easeIn",
+            },
+        },
+        animate: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                duration: defaultDuration,
+                ease: "easeOut",
+            },
+        },
+        bottom: {
+            y: defaultTranslate * -1,
+            opacity: 0,
+            transition: {
+                duration: defaultDuration,
+                ease: "easeIn",
+            },
+        },
+    },
 };
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,46 +1,57 @@
-import styles from './About.module.scss';
-import { motion } from 'framer-motion';
-import img from '../../assets/imgs/portrait-desat.jpg';
-import { transitions } from './About.transitions';
-import { Dispatch, SetStateAction } from 'react';
+import styles from "./About.module.scss";
+import { motion } from "framer-motion";
+import img from "../../assets/imgs/portrait-desat.jpg";
+import { transitions } from "./About.transitions";
+import { Dispatch, SetStateAction } from "react";
 
 interface AboutProps {
-	navDirection: 'up' | 'down';
-	isSubpathOpen: boolean;
-	setIsSubpathOpen: Dispatch<SetStateAction<boolean>>;
+    navDirection: "up" | "down";
+    isSubpathOpen: boolean;
+    setIsSubpathOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-export default function About({ navDirection, isSubpathOpen, setIsSubpathOpen }: AboutProps) {
-	return (
-		<motion.section
-			id={styles.About}
-			initial={navDirection === 'down' ? 'top' : 'bottom'}
-			animate='animate'
-			exit={navDirection === 'down' ? 'bottom' : 'top'}
-		>
-			<motion.section id={styles.landing} variants={transitions.landing}>
-				<div
-					className={`${styles.img} ${isSubpathOpen && styles.subpathOpen}`}
-					style={{ backgroundImage: `url(${img})` }}
-				></div>
-				<header>
-					<h1>About Me</h1>
-					{/* eslint-disable-next-line */}
-					<p>//</p>
-					<h2>
-						I'm a full-stack web developer specializing in the MERN stack. One year ago
-						I left my career as an accomplished business manager in the hospitality
-						industry to pursue becoming a developer.
-					</h2>
-					<button
-						className={styles.CTA}
-						onClick={() => setIsSubpathOpen((curr) => !curr)}
-					>
-						Read more
-					</button>
-				</header>
-			</motion.section>
-			<section></section>
-		</motion.section>
-	);
+export default function About({
+    navDirection,
+    isSubpathOpen,
+    setIsSubpathOpen,
+}: AboutProps) {
+    return (
+        <motion.section
+            id={styles.About}
+            initial={navDirection === "down" ? "top" : "bottom"}
+            animate="animate"
+            exit={navDirection === "down" ? "bottom" : "top"}
+        >
+            <motion.section id={styles.landing}>
+                <motion.div
+                    className={`${styles.img} ${
+                        isSubpathOpen && styles.subpathOpen
+                    }`}
+                    style={{ backgroundImage: `url(${img})` }}
+                    variants={transitions.transUp}
+                ></motion.div>
+                <motion.header variants={transitions.stagger}>
+                    <motion.h1 variants={transitions.transUp}>
+                        About Me
+                    </motion.h1>
+                    {/* eslint-disable-next-line */}
+                    <motion.p variants={transitions.transUp}>//</motion.p>
+                    <motion.h2 variants={transitions.transUp}>
+                        I'm a full-stack web developer specializing in the MERN
+                        stack. One year ago I left my career as an accomplished
+                        business manager in the hospitality industry to pursue
+                        becoming a developer.
+                    </motion.h2>
+                    <motion.button
+                        variants={transitions.transUp}
+                        className={styles.CTA}
+                        onClick={() => setIsSubpathOpen((curr) => !curr)}
+                    >
+                        Read more
+                    </motion.button>
+                </motion.header>
+            </motion.section>
+            <section></section>
+        </motion.section>
+    );
 }
