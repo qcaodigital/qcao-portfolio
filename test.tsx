d456732b098fc67ef000732311d4f4f229c5eb11
qcaodigital <qcao.digital@gmail.com>
2021-04-22 13:29:16 -0400

    remove test file & update readme

--- test.tsx
+++
@@ -1,7 +0,0 @@
-const quan = {
-	lastName: 'Cao',
-	languages: ['Javascript', 'Typescript', 'HTML', 'CSS'],
-	tools: [
-		'React, Node, Express, Mongo, NextJS, jQuery, SASS/SCSS, Webpack, Framer Motion, Photoshop',
-	],
-};
